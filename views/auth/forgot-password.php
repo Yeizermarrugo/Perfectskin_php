5e89159b4cd1eb5f7ed22fecc5bb0950e19c0782
Yeizermarrugo <yeizermarrugo@gmail.com>
2023-07-27 22:29:39 -0500

    [Include] Alerts templates

--- views/auth/forgot-password.php
+++ views/auth/forgot-password.php
@@ -1,6 +1,11 @@
 <h1 class="nombre-pagina">Olvide Password</h1>
 <p class="descripcion-pagina">Reestablecer Password</p>
 
+<?php 
+    include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+
 <form class="form" action="/forgot-password" method="post">
     <div class="campo">
         <label for="email">Email:</label>

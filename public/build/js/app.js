426232f4aaf1e50676052cdc36a13b7caaa2b115
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-01-25 13:45:06 -0500

    Add menu functionality

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,1 +1,82 @@
-let paso=1;function iniciarApp(){mostrarSeccion(),tabs()}function mostrarSeccion(){const t=document.querySelector(".mostrar");t&&t.classList.remove("mostrar");const o="#paso-"+paso;document.querySelector(o).classList.add("mostrar")}function tabs(){document.querySelectorAll(".tabs button").forEach(t=>{t.addEventListener("click",(function(t){paso=parseInt(t.target.dataset.paso),mostrarSeccion()}))})}document.addEventListener("DOMContentLoaded",(function(){iniciarApp()}));
+let paso = 1;
+const pasoInicial = 1,
+  pasoFinal = 3;
+function iniciarApp() {
+  mostrarSeccion(),
+    tabs(),
+    paginador(),
+    siguiente(),
+    anterior(),
+    consultarAPI();
+}
+function mostrarSeccion() {
+  const t = document.querySelector(".mostrar");
+  t && t.classList.remove("mostrar");
+  const e = "#paso-" + paso;
+  document.querySelector(e).classList.add("mostrar");
+  const o = document.querySelector(".actual");
+  o && o.classList.remove("actual");
+  document.querySelector(`[data-paso="${paso}"]`).classList.add("actual");
+}
+function tabs() {
+  document.querySelectorAll(".tabs button").forEach((t) => {
+    t.addEventListener("click", function (t) {
+      (paso = parseInt(t.target.dataset.paso)), mostrarSeccion(), paginador();
+    });
+  });
+}
+function paginador() {
+  const t = document.querySelector("#anterior"),
+    e = document.querySelector("#siguiente");
+  1 === paso
+    ? (t.classList.add("ocultar"), e.classList.remove("ocultar"))
+    : 3 === paso
+    ? (t.classList.remove("ocultar"), e.classList.add("ocultar"))
+    : (t.classList.remove("ocultar"), e.classList.remove("ocultar")),
+    mostrarSeccion();
+}
+function anterior() {
+  document.querySelector("#anterior").addEventListener("click", () => {
+    paso <= 1 || (paso--, paginador());
+  });
+}
+function siguiente() {
+  document.querySelector("#siguiente").addEventListener("click", () => {
+    paso >= 3 || (paso++, paginador());
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+async function consultarAPI() {
+  try {
+    const url = "http://localhost:3000/api/servicios";
+    const response = await fetch(url);
+    const servicios = await response.json();
+    mostrarServicios(servicios);
+  } catch (error) {
+    console.log("Error al realizar la petición: " + error);
+  }
+}
+function mostrarServicios(servicios) {
+  servicios.forEach((servicio) => {
+    const { id, name, price } = servicio;
+    const nombreServicio = document.createElement("P");
+    nombreServicio.classList.add("nombre-servicio");
+    nombreServicio.textContent = name;
+
+    const precioServicio = document.createElement("P");
+    precioServicio.classList.add("precio-servicio");
+    precioServicio.textContent = `$${price}`;
+
+    const servicioDiv = document.createElement("DIV");
+    servicioDiv.classList.add("servicio");
+    servicioDiv.dataset.idServicio = id;
+
+    servicioDiv.appendChild(nombreServicio);
+    servicioDiv.appendChild(precioServicio);
+
+    document.querySelector("#servicios").appendChild(servicioDiv);
+  });
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,87 @@
 let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
 
-document.addEventListener('DOMContentLoaded', function(){
-    iniciarApp();
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
 });
 
 function iniciarApp() {
-    mostrarSeccion(); //Muestra y oculta las secciones
-    tabs(); //Cambiar la seccion cuando se presionen los tabs
+  mostrarSeccion(); //Muestra y oculta las secciones
+  tabs(); //Cambiar la seccion cuando se presionen los tabs
+  paginador(); //Agrega o quita la paginacion
+  siguiente();
+  anterior()
 }
 
 function mostrarSeccion() {
+  //Ocultar seccion que tenga la clase de mostrar
+  const seccionAnterior = document.querySelector(".mostrar");
+  if (seccionAnterior) {
+    seccionAnterior.classList.remove("mostrar");
+  }
 
-    //Ocultar seccion que tenga la clase de mostrar
-    const seccionAnterior = document.querySelector('.mostrar');
-    if(seccionAnterior){
-        seccionAnterior.classList.remove('mostrar');
-    }
+  //Seleccionar la seccion con el paso...
+  const pasoSelector = `#paso-${paso}`;
+  const seccion = document.querySelector(pasoSelector);
+  seccion.classList.add("mostrar");
+  //Quitar la clase de actual al tab anterior
+  const btnAnterior = document.querySelector(".actual");
+  if (btnAnterior) {
+    btnAnterior.classList.remove("actual");
+  }
 
-    //Seleccionar la seccion con el paso...
-    const pasoSelector = `#paso-${paso}`;
-    const seccion = document.querySelector(pasoSelector);
-    seccion.classList.add('mostrar');
-
+  //Resaltar tab actual
+  const tabsContenedor = document.querySelector(`[data-paso="${paso}"]`);
+  tabsContenedor.classList.add("actual");
 }
 
 function tabs() {
-    const botones = document.querySelectorAll('.tabs button');
+  const botones = document.querySelectorAll(".tabs button");
 
-    botones.forEach( boton=> {
-        boton.addEventListener('click', function(e) {
-            paso = parseInt(e.target.dataset.paso);
-            mostrarSeccion();
-        });
+  botones.forEach((boton) => {
+    boton.addEventListener("click", function (e) {
+      paso = parseInt(e.target.dataset.paso);
+      mostrarSeccion();
+      paginador();
+    });
+  });
+}
+
+function paginador() {
+    const anterior = document.querySelector("#anterior");
+    const siguiente = document.querySelector("#siguiente");
+
+    if(paso === 1){
+        anterior.classList.add('ocultar');
+        siguiente.classList.remove('ocultar');
+    }else if(paso === 3){
+        anterior.classList.remove('ocultar');
+        siguiente.classList.add('ocultar');
+    }else{
+        anterior.classList.remove('ocultar');
+        siguiente.classList.remove('ocultar');
+    }
+    mostrarSeccion();
+}
+
+function anterior() {
+    const anterior = document.querySelector('#anterior')
+    anterior.addEventListener('click', ()=>{
+        if(paso<=pasoInicial) return;
+        paso--;
+        paginador();
+        })
+}
+
+
+function siguiente() {
+    const siguiente = document.querySelector('#siguiente');
+    siguiente.addEventListener('click', ()=> {
+        if(paso>=pasoFinal) return;
+        paso++;
+        paginador();
     });
 }
+
+

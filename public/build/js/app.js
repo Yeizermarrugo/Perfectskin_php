77c435f8d2bdc3725527b3fb564b499087bbbe81
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-01-30 22:50:06 -0500

    [ADD] functionality to select Service, date and hour for the service

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,38 +1,42 @@
 let paso = 1;
 const pasoInicial = 1,
-  pasoFinal = 3;
+  pasoFinal = 3,
+  cita = { id: "", name: "", hora: "", fecha: "", servicios: [] };
 function iniciarApp() {
   mostrarSeccion(),
     tabs(),
     paginador(),
     siguiente(),
     anterior(),
-    consultarAPI();
+    consultarAPI(),
+    nombreCliente(),
+    seleccionarFecha(),
+    seleccionarHora();
 }
 function mostrarSeccion() {
-  const t = document.querySelector(".mostrar");
-  t && t.classList.remove("mostrar");
-  const e = "#paso-" + paso;
-  document.querySelector(e).classList.add("mostrar");
-  const o = document.querySelector(".actual");
-  o && o.classList.remove("actual");
+  const e = document.querySelector(".mostrar");
+  e && e.classList.remove("mostrar");
+  const t = "#paso-" + paso;
+  document.querySelector(t).classList.add("mostrar");
+  const a = document.querySelector(".actual");
+  a && a.classList.remove("actual");
   document.querySelector(`[data-paso="${paso}"]`).classList.add("actual");
 }
 function tabs() {
-  document.querySelectorAll(".tabs button").forEach((t) => {
-    t.addEventListener("click", function (t) {
-      (paso = parseInt(t.target.dataset.paso)), mostrarSeccion(), paginador();
+  document.querySelectorAll(".tabs button").forEach((e) => {
+    e.addEventListener("click", function (e) {
+      (paso = parseInt(e.target.dataset.paso)), mostrarSeccion(), paginador();
     });
   });
 }
 function paginador() {
-  const t = document.querySelector("#anterior"),
-    e = document.querySelector("#siguiente");
+  const e = document.querySelector("#anterior"),
+    t = document.querySelector("#siguiente");
   1 === paso
-    ? (t.classList.add("ocultar"), e.classList.remove("ocultar"))
+    ? (e.classList.add("ocultar"), t.classList.remove("ocultar"))
     : 3 === paso
-    ? (t.classList.remove("ocultar"), e.classList.add("ocultar"))
-    : (t.classList.remove("ocultar"), e.classList.remove("ocultar")),
+    ? (e.classList.remove("ocultar"), t.classList.add("ocultar"))
+    : (e.classList.remove("ocultar"), t.classList.remove("ocultar")),
     mostrarSeccion();
 }
 function anterior() {
@@ -45,38 +49,108 @@ function siguiente() {
     paso >= 3 || (paso++, paginador());
   });
 }
-document.addEventListener("DOMContentLoaded", function () {
-  iniciarApp();
-});
-
 async function consultarAPI() {
   try {
-    const url = "http://localhost:3000/api/servicios";
-    const response = await fetch(url);
-    const servicios = await response.json();
-    mostrarServicios(servicios);
-  } catch (error) {
-    console.log("Error al realizar la petición: " + error);
+    const e = "http://localhost:3000/api/servicios",
+      t = await fetch(e);
+    mostrarServicios(await t.json());
+  } catch (e) {
+    console.log(e);
   }
 }
-function mostrarServicios(servicios) {
-  servicios.forEach((servicio) => {
-    const { id, name, price } = servicio;
-    const nombreServicio = document.createElement("P");
-    nombreServicio.classList.add("nombre-servicio");
-    nombreServicio.textContent = name;
-
-    const precioServicio = document.createElement("P");
-    precioServicio.classList.add("precio-servicio");
-    precioServicio.textContent = `$${price}`;
-
-    const servicioDiv = document.createElement("DIV");
-    servicioDiv.classList.add("servicio");
-    servicioDiv.dataset.idServicio = id;
-
-    servicioDiv.appendChild(nombreServicio);
-    servicioDiv.appendChild(precioServicio);
-
-    document.querySelector("#servicios").appendChild(servicioDiv);
+function mostrarServicios(e) {
+  e.forEach((e) => {
+    const { id: t, name: a, price: o } = e,
+      c = document.createElement("P");
+    c.classList.add("nombre-servicio"), (c.textContent = a);
+    const n = document.createElement("P");
+    n.classList.add("precio-servicio"), (n.textContent = "" + o);
+    const r = document.createElement("DIV");
+    r.classList.add("servicio"),
+      (r.dataset.idServicio = t),
+      (r.onclick = function () {
+        seleccionarServicio(e);
+      }),
+      r.appendChild(c),
+      r.appendChild(n),
+      document.querySelector("#servicios").appendChild(r);
   });
 }
+function seleccionarServicio(e) {
+  const { id: t } = e,
+    { servicios: a } = cita,
+    o = document.querySelector(`[data-id-servicio="${t}"]`);
+  if (a.some((e) => e.id === t))
+    (cita.servicios = a.filter((e) => e.id !== t)),
+      o.classList.remove("seleccionado");
+  else {
+    if (a.length > 0)
+      return void alert("Solo puedes tomar un servicio a la vez");
+    (cita.servicios = [...a, e]), o.classList.add("seleccionado");
+  }
+}
+function nombreCliente() {
+  cita.name = document.querySelector("#nombre").value;
+}
+function seleccionarFecha() {
+  const e = document.querySelector("#fecha"),
+    t = document.querySelector("#hora");
+  let a = !1;
+  e.addEventListener("input", function (e) {
+    t.value = "";
+    const o = new Date(e.target.value).getUTCDay();
+    [0].includes(o)
+      ? (a ||
+          ((a = !0),
+          mostrarAlerta("Domingo no hay servicio disponible", "error")),
+        (e.target.value = ""))
+      : [6].includes(o)
+      ? (a ||
+          ((a = !0),
+          mostrarAlerta("Sabado atendemos de 09:00 AM a 03:00 PM", "warning")),
+        (cita.fecha = e.target.value))
+      : ((a = !1), (cita.fecha = e.target.value));
+  });
+}
+function seleccionarHora() {
+  const e = document.querySelector("#hora"),
+    t = document.querySelector("#fecha");
+  let a = !1;
+  e.addEventListener("input", function (o) {
+    const c = new Date(t.value).getUTCDay(),
+      n = o.target.value,
+      r = parseInt(n.split(":")[0]);
+    6 === c && (r < 9 || r > 15)
+      ? (a ||
+          ((a = !0),
+          mostrarAlerta(
+            "La hora debe ser entre las 09:00 AM y 3:00 PM",
+            "error"
+          )),
+        (e.value = ""))
+      : ((a = !1), (cita.hora = o.target.value)),
+      console.log(cita);
+  });
+}
+function mostrarAlerta(e, t) {
+  const a = document.createElement("DIV");
+  (a.textContent = e), a.classList.add("alerta"), a.classList.add(t);
+  document.querySelector("#paso-2 p").appendChild(a),
+    setTimeout(() => {
+      a.classList.add("desvanecer");
+    }, 3e3),
+    a.addEventListener("transitionend", () => {
+      a.remove();
+    });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp(),
+    flatpickr("#hora", {
+      enableTime: !0,
+      noCalendar: !0,
+      dateFormat: "H:i",
+      minuteIncrement: 60,
+      minTime: "08:00",
+      maxTime: "17:00",
+    });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,25 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita = {
+  id: "",
+  name: "",
+  hora: "",
+  fecha: "",
+  servicios: [],
+};
+
 document.addEventListener("DOMContentLoaded", function () {
+  
   iniciarApp();
+  flatpickr('#hora', {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: 'H:i',
+    minuteIncrement: 60, // Fija los minutos en 00
+    minTime: '08:00',
+    maxTime: '17:00',
+});
 });
 
 function iniciarApp() {
@@ -11,7 +28,11 @@ function iniciarApp() {
   tabs(); //Cambiar la seccion cuando se presionen los tabs
   paginador(); //Agrega o quita la paginacion
   siguiente();
-  anterior()
+  anterior();
+  consultarAPI();
+  nombreCliente();
+  seleccionarFecha();
+  seleccionarHora();
 }
 
 function mostrarSeccion() {
@@ -38,7 +59,7 @@ function mostrarSeccion() {
 
 function tabs() {
   const botones = document.querySelectorAll(".tabs button");
-
+  
   botones.forEach((boton) => {
     boton.addEventListener("click", function (e) {
       paso = parseInt(e.target.dataset.paso);
@@ -49,39 +70,184 @@ function tabs() {
 }
 
 function paginador() {
-    const anterior = document.querySelector("#anterior");
-    const siguiente = document.querySelector("#siguiente");
-
-    if(paso === 1){
-        anterior.classList.add('ocultar');
-        siguiente.classList.remove('ocultar');
-    }else if(paso === 3){
-        anterior.classList.remove('ocultar');
-        siguiente.classList.add('ocultar');
-    }else{
-        anterior.classList.remove('ocultar');
-        siguiente.classList.remove('ocultar');
-    }
-    mostrarSeccion();
+  const anterior = document.querySelector("#anterior");
+  const siguiente = document.querySelector("#siguiente");
+  
+  if (paso === 1) {
+    anterior.classList.add("ocultar");
+    siguiente.classList.remove("ocultar");
+  } else if (paso === 3) {
+    anterior.classList.remove("ocultar");
+    siguiente.classList.add("ocultar");
+  } else {
+    anterior.classList.remove("ocultar");
+    siguiente.classList.remove("ocultar");
+  }
+  mostrarSeccion();
 }
 
 function anterior() {
-    const anterior = document.querySelector('#anterior')
-    anterior.addEventListener('click', ()=>{
-        if(paso<=pasoInicial) return;
-        paso--;
-        paginador();
-        })
+  const anterior = document.querySelector("#anterior");
+  anterior.addEventListener("click", () => {
+    if (paso <= pasoInicial) return;
+    paso--;
+    paginador();
+  });
 }
-
 
 function siguiente() {
-    const siguiente = document.querySelector('#siguiente');
-    siguiente.addEventListener('click', ()=> {
-        if(paso>=pasoFinal) return;
-        paso++;
-        paginador();
-    });
+  const siguiente = document.querySelector("#siguiente");
+  siguiente.addEventListener("click", () => {
+    if (paso >= pasoFinal) return;
+    paso++;
+    paginador();
+  });
 }
 
+async function consultarAPI() {
+  try {
+    const url = "http://localhost:3000/api/servicios";
+    const resultado = await fetch(url);
+    const servicios = await resultado.json();
+    mostrarServicios(servicios);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function mostrarServicios(servicios) {
+  servicios.forEach((servicio) => {
+    const { id, name, price } = servicio;
+    
+    const nombreServicio = document.createElement("P");
+    nombreServicio.classList.add("nombre-servicio");
+    nombreServicio.textContent = name;
+    
+    const precioServicio = document.createElement("P");
+    precioServicio.classList.add("precio-servicio");
+    precioServicio.textContent = `${price}`;
+    
+    const servicioDiv = document.createElement("DIV");
+    servicioDiv.classList.add("servicio");
+    servicioDiv.dataset.idServicio = id;
+    servicioDiv.onclick = function () {
+      seleccionarServicio(servicio);
+    };
+    
+    servicioDiv.appendChild(nombreServicio);
+    servicioDiv.appendChild(precioServicio);
+    
+    document.querySelector("#servicios").appendChild(servicioDiv);
+  });
+}
+
+function seleccionarServicio(servicio) {
+  const { id } = servicio;
+  const { servicios } = cita;
+  const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+
+  // Comprobar si el servicio ya fue agregado
+  const servicioYaAgregado = servicios.some((agregado) => agregado.id === id);
+
+  if (servicioYaAgregado) {
+    // Si el servicio ya está seleccionado, removerlo
+    cita.servicios = servicios.filter((agregado) => agregado.id !== id);
+    divServicio.classList.remove("seleccionado");
+  } else {
+    // Si no está seleccionado, agregarlo
+    if (servicios.length > 0) {
+      // Si ya hay un servicio seleccionado, mostrar alerta
+      alert("Solo puedes tomar un servicio a la vez");
+      return;
+    }
+    
+    cita.servicios = [...servicios, servicio];
+    divServicio.classList.add("seleccionado");
+  }
+  
+}
+
+function nombreCliente() {
+  cita.name = document.querySelector("#nombre").value;
+}
+
+
+    function seleccionarFecha() {
+      const inputFecha = document.querySelector("#fecha");
+      const inputHora = document.querySelector("#hora");
+      let alertaMostrada = false;
+
+      inputFecha.addEventListener("input", function (e) {
+        inputHora.value = '';
+        const dia = new Date(e.target.value).getUTCDay();
+        
+        if ([0].includes(dia)) {
+          // Solo mostrar la alerta si no se ha mostrado antes
+          if (!alertaMostrada) {
+            alertaMostrada = true;
+            mostrarAlerta("Domingo no hay servicio disponible", "error");
+          }
+          e.target.value = "";
+        }else if([6].includes(dia)){
+          if (!alertaMostrada) {
+            alertaMostrada = true;
+            mostrarAlerta("Sabado atendemos de 09:00 AM a 03:00 PM", "warning");
+          }
+          cita.fecha = e.target.value;
+        }
+        else {
+          // Restablecer la variable cuando la fecha es válida
+          alertaMostrada = false;
+          cita.fecha = e.target.value;
+        }
+      });
+    }
+ 
+    function seleccionarHora() {
+      const inputHora = document.querySelector('#hora');
+      const inputFecha = document.querySelector("#fecha");
+      let alertaMostrada = false;
+      
+      inputHora.addEventListener('input', function (event) {
+    const fechaSeleccionada = new Date(inputFecha.value);
+    const dia = fechaSeleccionada.getUTCDay();
+    const horaCita = event.target.value;
+    const hora = parseInt(horaCita.split(':')[0]);
+
+    if (dia === 6 && (hora < 9 || hora > 15)) {
+      // Mostrar la alerta si no se ha mostrado antes
+      if (!alertaMostrada) {
+        alertaMostrada = true;
+        mostrarAlerta(`La hora debe ser entre las 09:00 AM y 3:00 PM`, 'error');
+      }
+      inputHora.value = '';
+      // No limpiar el campo de hora para permitir al usuario elegir otra hora
+    } else {
+      alertaMostrada = false;
+      cita.hora = event.target.value;
+    }
+    console.log(cita);
+  });
+}
+
+
+
+function mostrarAlerta(mensaje, tipo) {
+  const alerta = document.createElement("DIV");
+  alerta.textContent = mensaje;
+  alerta.classList.add("alerta");
+  alerta.classList.add(tipo);
+
+  const formulario = document.querySelector("#paso-2 p");
+  formulario.appendChild(alerta);
+
+  setTimeout(() => {
+    alerta.classList.add("desvanecer");
+  }, 3000);
+
+  // Eliminar el elemento después de completarse la transición
+  alerta.addEventListener("transitionend", () => {
+    alerta.remove();
+  });
+}
 

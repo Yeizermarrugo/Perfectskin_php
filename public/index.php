45fc1c22a65282032bd57c4c544acb779c907689
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-06 00:32:58 -0500

    [MOD] Controller name and [ADD] API to validate the time of an appointment by date

--- public/index.php
+++ public/index.php
@@ -4,10 +4,12 @@ require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\APIController;
 use Controllers\LoginController;
-use Controllers\AppointmentController;
+use Controllers\CitaController;
+use Model\Cita;
 use MVC\Router;
 
 $router = new Router();
+
 
 //Iniciar Sesion
 $router->get('/', [LoginController::class, 'login']);
@@ -29,10 +31,12 @@ $router->get('/confirm-account', [LoginController::class, 'confirm']);
 $router->get('/message', [LoginController::class, 'message']);
 
 //Area privada
-$router->get('/appointment', [AppointmentController::class, 'index']);
+$router->get('/cita', [CitaController::class, 'index']);
 
 //API citas
 $router->get('/api/servicios', [APIController::class, 'index']);
+$router->get('/api/citasPorFecha', [APIController::class, 'citasPorFecha']);
+$router->post('/api/citas', [APIController::class, 'guardar']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();

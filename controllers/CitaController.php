0e318431de61731d7dfae25044c29387e85a5e6f
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-13 08:44:44 -0500

    [ADD] function to protect route

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,6 +6,12 @@ use MVC\Router;
 
 class CitaController {
     public static function index(Router $router) {
+
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+        isAuth();
         
         $router->render('cita/index', [
             'name'=> $_SESSION['name'],

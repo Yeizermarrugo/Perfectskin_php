e8ffcccf09edf79b898489a011869b0fd11057bc
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-14 00:12:57 -0500

    [ADD] Join query selector to get appointment by date

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -151,6 +151,12 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+    //Consulta plana de SQL(Utilizar cuando los metodos del modelo no son suficientes)
+    public static function SQL($query) {
+        $resultado = self::consultarSQL($query);
+        return  $resultado;
+    }
+
     // crea un nuevo registro
     public function crear() {
         // Sanitizar los datos

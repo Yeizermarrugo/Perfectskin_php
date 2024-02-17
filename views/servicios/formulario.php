e1f7f84de81f9a3c758db825050fd86af8fdc8cc
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-17 08:56:17 -0500

    [CREATE] Form to add new service

---
+++ views/servicios/formulario.php
@@ -0,0 +1,21 @@
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input 
+        type="text"
+        id="nombre"
+        placeholder="Nombre Servicio"
+        name="name"
+        value="<?php echo $servicio->name; ?>"
+    />
+</div>
+
+<div class="campo">
+    <label for="precio">Precio</label>
+    <input 
+        type="number"
+        id="precio"
+        placeholder="Precio Servicio"
+        name="price"
+        value="<?php echo $servicio->price; ?>"
+    />
+</div>

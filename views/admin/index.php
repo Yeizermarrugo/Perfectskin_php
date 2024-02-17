b4db351f2ac2569f6a72d450d45caf32f1921b11
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-17 14:45:28 -0500

    [MOD] show appointments deleted

--- views/admin/index.php
+++ views/admin/index.php
@@ -3,7 +3,6 @@
 ?>
 <h1 class="nombre-pagina">Panel de Administración</h1>
 
-
 <h2>Buscar Citas</h2>
 <div class="busqueda">
     <form class="formulario">
@@ -27,43 +26,33 @@
 
 <div id="citas-admin">
     <ul class="citas">   
-            <?php 
-                $idCita = 0;
-                foreach( $citas as $key => $cita ) {
-   
-                    if($idCita !== $cita->id) {
-                        $total = 0;
-            ?>
-            <li>
-                    <p>ID: <span><?php echo $cita->id; ?></span></p>
-                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
-                    <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
-                    <p>Email: <span><?php echo $cita->email; ?></span></p>
-                    <p>Telefono: <span><?php echo $cita->phone; ?></span></p>
+        <?php 
+            foreach( $citas as $cita ) {
+        ?>
+        <li>
+            <p>ID: <span><?php echo $cita->id; ?></span></p>
+            <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+            <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+            <p>Email: <span><?php echo $cita->email; ?></span></p>
+            <p>Telefono: <span><?php echo $cita->phone; ?></span></p>
 
-                    <h3>Servicio</h3>
-            <?php 
-                $idCita = $cita->id;
-            } // Fin de IF 
-                $total += $cita->price;
-            ?>
-                    <p class="servicio"><?php echo $cita->servicio; ?></p>
-            
-            <?php 
-                $actual = $cita->id;
-                $proximo = $citas[$key + 1]->id ?? 0;
+            <h3>Servicio</h3>
+            <p class="servicio"><?php echo $cita->servicio; ?></p>
+            <p class="total">Total: <span>$ <?php echo $cita->price; ?></span></p>
 
-                if(esUltimo($actual, $proximo)) { ?>
-                    <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
-
-                    <form action="/api/eliminar" method="POST">
-                        <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
-                        <input type="submit" class="boton-eliminar" value="Eliminar">
-                    </form>
-
-            <?php } 
-          } // Fin de Foreach ?>
-     </ul>
+            <?php if($cita->eliminada == 1) { ?>
+                <p class='eliminada-mensaje'>Esta cita está marcada como eliminada.</p>
+            <?php } else { ?>
+                <form action="/api/eliminar" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
+                    <input type="submit" class="boton-eliminar" value="Eliminar">
+                </form>
+            <?php } ?>
+        </li>
+        <?php
+            }
+        ?>
+    </ul>
 </div>
 
 <?php

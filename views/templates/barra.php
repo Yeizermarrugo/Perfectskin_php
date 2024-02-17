2c8212f978b49d494094e51808f2f6d00d4b5709
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-17 08:54:08 -0500

    [ADD] Admin options

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -7,3 +7,11 @@
         </a>
     </div>
 </div>
+
+<?php if(isset($_SESSION['admin'])) { ?>
+    <div class="barra-servicios">
+        <a class="boton" href="/admin">Ver Citas</a>
+        <a class="boton" href="/servicios">Ver Servicios</a>
+        <a class="boton" href="/servicios/crear">Nuevo Servicio</a>
+    </div>
+<?php } ?>

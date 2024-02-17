f835e3a8b2bc4434518d590f9774fc30a6d71223
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-02-17 16:50:22 -0500

    [ADD] build

--- gulpfile.js
+++ gulpfile.js
@@ -62,4 +62,5 @@ function watchArchivos() {
   
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript,  imagenes, versionWebp,  watchArchivos ); 
+exports.default = parallel(css, javascript,  imagenes, versionWebp,  watchArchivos );
+exports.build = parallel(css, javascript,  imagenes, versionWebp );
